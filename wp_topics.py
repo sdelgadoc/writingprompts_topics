6009b1ff699826fd8d4a0e82fb1ed110b46b081e
Ubuntu <ubuntu@ip-172-30-0-224.us-west-2.compute.internal>
2016-02-29 21:51:59 +0000

    Changed text delimeter to a pipe

--- wp_topics.py
+++ wp_topics.py
@@ -17,7 +17,7 @@ subreddit = reddit.get_subreddit(subreddit_name)
 output_file = open("subreddit_output.txt", "w")
 
 for submission in subreddit.get_top_from_all(limit=post_count):
-    output_file.write(submission.title.encode('ascii', 'ignore') + " :;: " + str(submission.score) + " :;: " + str(submission.id) + "\n")
+    output_file.write(submission.title.encode('ascii', 'ignore') + "|" + str(submission.score) + "|" + str(submission.id) + "\n")
 
 output_file.close()
 
