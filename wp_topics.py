cf818d1ec7e3de52c6f61ab8c08f57b6d11742b9
Ubuntu <ubuntu@ip-172-30-0-224.us-west-2.compute.internal>
2016-02-29 20:44:38 +0000

    Completed wp_topics.py code, and added subreddit_output.txt file

---
+++ wp_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+## This script uses the Reddit API to collect post titles and scores in a particular subreddit
+
+# Import libraries
+import praw
+
+# Generate Reddit praw object
+user_agent = "Writing prompts topic analysis 1.0"
+reddit = praw.Reddit(user_agent=user_agent)
+
+# Save hot ~1000 post information
+subreddit_name = "WritingPrompts" 
+post_count = 1000
+
+subreddit = reddit.get_subreddit(subreddit_name)
+output_file = open("subreddit_output.txt", "w")
+
+for submission in subreddit.get_hot(limit=post_count):
+    output_file.write(submission.title.encode('ascii', 'ignore') + " :;: " + str(submission.score) + " :;: " + str(submission.id) + "\n")
+
+output_file.close()
+
